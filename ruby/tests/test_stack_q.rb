c3f96e0ead6a58fe0ef8178f1845d997584da933
Takashi Tamura <tamuratak@users.noreply.github.com>
2015-12-27 12:42:54 +0900

    ditto.

--- ruby/tests/test_stack_q.rb
+++ ruby/tests/test_stack_q.rb
@@ -160,7 +160,8 @@ sinalart : if not emptyp( intersection({sin2, sin3, sin4, sin5, cos2, cos3, cos4
 fxalart_set : intersection({x, y, s, t, fx, fy, fxx, fxy, fyx, fyy}, setify(listofops(ans1)));
 fxalart_elem : if not emptyp( fxalart_set ) then listify(fxalart_set)[1];
 fxalart : if not emptyp( fxalart_set ) then 1 else false;
-does_hold(ex) := is( ratsimp( radcan( exponentialize(ex) ) ) );
+stackqsimp(ex) := ratsimp( radcan( exponentialize(ex) ) );
+does_hold(ex) := is( stackqsimp(ex) );
 ans1 : ratsubst(fxy, fyx, ans1);
 a1 : ZZZ;
 result : if does_hold( a1 = ans1 ) then 1 else false;
