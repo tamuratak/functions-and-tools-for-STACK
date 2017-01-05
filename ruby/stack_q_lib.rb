90bacd86d79cf09076f83f88068784c6a92cc7c7
Takashi Tamura <tamuratak@users.noreply.github.com>
2017-01-05 19:12:52 +0900

    add class Is_same_linear_eq and class AlgEquiv

--- ruby/stack_q_lib.rb
+++ ruby/stack_q_lib.rb
@@ -64,20 +64,26 @@ class STACK_Q
 
     case mthd
     when "AlgEquiv", "CasEqualNotAsin"
-      stack_mthd = mthd
-      t_ans1 = cdata(a1)
-      feedbk = feedback(mthd, a1)
-      if mthd == "CasEqualNotAsin"
-        stack_mthd = "CasEqual"
-        forbidwords = ",asin,acos,atan"
-      end
-    when "is_same_interval",  "is_same_linear_eq", "has_same_nullspace", "has_same_deriv", "does_satisfy"
+      quiz = AlgEquiv.new(a1, input_size: input_size, mthd: mthd)
+      t_ans1 = quiz.t_ans1
+      feedbk = quiz.feedbk
+      stack_mthd = quiz.stack_mthd
+      forbidwords = quiz.forbidwords
+#      input_type = quiz.input_type
+#      input_size = quiz.input_size
+    when "is_same_interval",  "is_same_linear_eq", "has_same_nullspace", "is_same_plane"
+      quiz = Is_same_linear_eq.new(a1, input_size: input_size, mthd: mthd)
+      t_ans1 = quiz.t_ans1
+      feedbk = quiz.feedbk
+      stack_mthd = quiz.stack_mthd
+      forbidwords = quiz.forbidwords
+      input_type = quiz.input_type
+      input_size = quiz.input_size
+    when "has_same_deriv", "does_satisfy"
       stack_mthd = "CasEqual"
       t_ans1 = cdata(a1)
       feedbk = feedback(mthd, a1, ext)
       case mthd
-      when "is_same_linear_eq"
-        eq_type_check(a1, line_num)
       when "has_same_deriv"
         stack_mthd = "AlgEquiv"
       end
@@ -340,11 +346,35 @@ EOS
 module StackqUtil
 include ERB::Util
 
+  def cdata(s)
+    "<![CDATA[" + esq_cdata(s) + "]]>"
+  end
+
+  def esq_cdata(s)
+    (s || "").gsub("]]>", "]]]]><![CDATA[>")
+  end
+
   def does_hold_mac
     <<EOS.chomp
 stackqsimp(ex) := fullratsimp( radcan( factcomb( exponentialize(ex) ) ) );
 does_hold(ex) := is( stackqsimp(lhs(ex)-rhs(ex)=0) or ratsimp(ex) );
 declare(n, integer);
+EOS
+  end
+
+  def feedbk_alart
+    fun_num_list = ["sin", "cos", "tan", "asin", "acos", "atan", "exp", "log"].product((0..9).to_a).map(&:join).join(", ")
+    fdbk_alart = <<EOS.chomp
+listofops(x) := block([], if not atom(x) then cons( op(x), flatten(map(listofops, args(x))) ) else [] );
+xyalart_set : intersection({xy, yx}, setify( append(listofvars(ans1), listofops(ans1))   ));
+xyalart_elem : if not emptyp( xyalart_set ) then listify(xyalart_set)[1];
+xyalart : if not emptyp( xyalart_set ) then 1 else false;
+sinalart : if not emptyp( intersection({#{fun_num_list}}, setify(listofvars(ans1))) ) then 1 else false;
+fxalart_set : intersection({x, y, s, t, fx, fy, fxx, fxy, fyx, fyy}, setify(listofops(ans1)));
+fxalart_elem : if not emptyp( fxalart_set ) then listify(fxalart_set)[1];
+fxalart : if not emptyp( fxalart_set ) then 1 else false;
+#{does_hold_mac}
+ans1 : ratsubst(fxy, fyx, ans1);
 EOS
   end
 
@@ -434,13 +464,33 @@ include StackqUtil
 class StackqBase
   include StackqUtil
 
-  def initialize(a1=nil, input_size: 15, line_num: nil)
+  def initialize(a1=nil, input_size: 15, line_num: nil, mthd: mthd)
     @a1 = a1
     @input_size = input_size
     @line_num = line_num
+    @mthd = mthd
   end
   attr_accessor :mthd
 
+  def t_ans1
+    cdata(@a1)
+  end
+
+  def stack_mthd
+    "CasEqual"
+  end
+
+  def forbidwords
+    ""
+  end
+
+  def input_type
+    "algebraic"
+  end
+
+  def input_size
+    100
+  end
 end
 
 class Is_P_and_PAP < StackqBase
@@ -719,6 +769,127 @@ HERE
     ret
   end
 
+end
+
+class AlgEquiv < StackqBase
+  def initialize(*args)
+    super
+  end
+
+  def t_ans1
+    cdata(@a1)
+  end
+
+  def feedbk
+    <<EOS.chomp
+<![CDATA[
+#{feedbk_alart}
+a1 : #{esq_cdata(@a1)};
+result : if does_hold( a1 = ans1 ) then 1 else false;
+]]>
+EOS
+  end
+
+  def stack_mthd
+    if @mthd == "CasEqualNotAsin"
+      "CasEqual"
+    else
+      @mthd
+    end
+  end
+
+  def forbidwords
+    if @mthd == "CasEqualNotAsin"
+      ",asin,acos,atan"
+    else
+      ""
+    end
+  end
+end
+
+class Is_same_linear_eq < StackqBase
+  def initialize(*args)
+    super
+    eq_type_check(@a1, @line_num) if @mthd == "is_same_linear_eq"
+  end
+
+  def eq_type_check(s, line_num)
+    unless /\A\{[^\{\}]*?\}\z/ =~ s
+      @err_msg = "error at line: #{line_num}" + "\n" + "invalid answer type"
+      raise "invalid answer type"
+    end
+  end
+
+  def t_ans1
+    case @mthd
+    when "is_same_plane"
+      cdata("transpose(matrix(" + @a1 + "))")
+    else
+      cdata(@a1)
+    end
+  end
+
+  def feedbk
+    ret = ""
+    ret <<
+<<EOS
+<![CDATA[
+is_same_linear_space(a, x) := block([ret, a0, x0, am, xm, am_dim, i],ret : true,a0 : listify(radcan(a)),x0 : listify(radcan(x)),am : apply(matrix, a0),xm : apply(matrix, x0),ret: ret and is(rank(am) = rank(xm)),if ret then (am_dim : rank(am),for i:1 thru length(x0) do (m : apply(matrix, cons(x0[i], a0)),ret : ret and is(rank(m) = am_dim))),ret);
+basis_of_plane(v) := block([params],params : listofvars(v),map(lambda([v1], diff(v, v1)), params));
+pos_of_plane(v) := block([v0 : v, params, i],params : listofvars(v),for i:1 thru length(params) do v0 : subst(0, params[i], v0),v0);
+is_on_plane(p, v) := block([eq],eq : map("=", makelist(0, length(v)), v-p),is(not ([] = solve(eq, listofvars(v)))));
+is_same_plane(v1, v2) := block([b1, b2, p1, p2, ret : true],b1 : basis_of_plane(v1),b2 : basis_of_plane(v2),ret : ret and is_same_linear_space(b1, b2),ret : ret and is_on_plane(pos_of_plane(v1), v2),ret : ret and is_on_plane(pos_of_plane(v2), v1));
+eq_to_param(eq) := block([params, tmp],eq : listify(eq),params : sort(listofvars(eq)),tmp : solve(eq, params),subst(tmp[1], params));
+is_same_linear_eq(eq1, eq2) := block([pa1, pa2],pa1 : eq_to_param(eq1),pa2 : eq_to_param(eq2),is_same_plane(pa1, pa2));
+
+EOS
+      # ]]> should be added in the following.
+    case @mthd
+    when "is_same_linear_eq"
+      ret <<
+<<EOS.chomp
+a1 : #{esq_cdata(@a1)};
+result : if is_same_linear_eq(a1, ans1) then 1 else false;
+]]>
+EOS
+    when "is_same_plane"
+      ret <<
+<<EOS.chomp
+a1 : #{esq_cdata(@a1)};
+ans1 : list_matrix_entries(ans1);
+result : if is_same_plane(a1, ans1) then 1 else false;
+]]>
+EOS
+    when "has_same_nullspace"
+      ret <<
+<<EOS.chomp
+a1 : #{esq_cdata(@a1)};
+result : if is_same_linear_space(args(a1), args(ans1)) then 1 else false;
+]]>
+EOS
+    else
+      raise 
+    end
+    return ret
+  end
+
+  def input_type
+    case @mthd
+    when "is_same_plane"
+      "matrix"
+    else
+      super
+    end
+  end
+
+  def input_size
+    case @mthd
+    when "is_same_plane"
+      15
+    else
+      100
+    end
+  end
 end
 
 end
