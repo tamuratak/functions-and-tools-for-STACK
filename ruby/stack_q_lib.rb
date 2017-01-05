0d806a39505ecc9f40e5d96566d9eca783db554c
Takashi Tamura <tamuratak@users.noreply.github.com>
2017-01-05 13:35:36 +0900

    add class Is_same_eigenval_and_eigenvec and module BasisUtil

--- ruby/stack_q_lib.rb
+++ ruby/stack_q_lib.rb
@@ -135,10 +135,15 @@ class STACK_Q
     when "is_same_eigenval_and_eigenvec", "is_same_eigenval_and_orthonormal_eigenvec"
       input_size = @opt["form-size"] || 15
       x = ERB.new(TMPL_eigen, nil, '-')
-      eigen_val_num, dim = eigen_num_dim(a1)
-      ans_inputs = eigen_ans_inputs(eigen_val_num, dim, input_size)
-      feedbk = eigen_feedback(eigen_val_num, dim, mthd)
-      ans_forms = eigen_forms(eigen_val_num, dim)
+      quiz = Is_same_eigenval_and_eigenvec.new(a1)
+      quiz.mthd = mthd
+      ans_inputs = quiz.ans_inputs
+      feedbk = quiz.feedbk
+      ans_forms = quiz.ans_forms
+#       eigen_val_num, dim = eigen_num_dim(a1)
+#       ans_inputs = eigen_ans_inputs(eigen_val_num, dim, input_size)
+#       feedbk = eigen_feedback(eigen_val_num, dim, mthd)
+#       ans_forms = eigen_forms(eigen_val_num, dim)
     else
       return nil
     end
@@ -365,69 +370,6 @@ EOS
   def eigen_ans_arry(dim, idx)
     "[" + n_join(dim, "ans#{idx}_%d") + "]"
   end
-
-  def eigen_feedback(eigen_val_num, dim, mthd = "")
-    ans_vals = n_join(eigen_val_num, "ans_val%d") # ans_val == ans_eigenval
-    large_Ns = n_join(dim, "N", ", ")
-    ERB.new(<<HERE, nil, '-').result(binding).chomp
-<![CDATA[
-<%= basis_feedback_lib_mac() %>
-ith : 0;
-result : is(<%= eigen_val_num %> = length(unique([<%= ans_vals %>])));
-ith : if result then ith + 1 else ith;
-<%- (1..eigen_val_num).each do |i| -%>
-vec<%= i %> : delete([<%= large_Ns %>], maplist(list_matrix_entries, <%= eigen_ans_arry(dim,i) %>));
-kvec<%= i %> : assoc(ans_val<%= i %>, k1);
-result : result and listp(kvec<%= i %>) and <%= basis_chk(mthd) %>(vec<%= i %>) and is_same_linear_space(kvec<%= i %>, vec<%= i %>);
-ith : if result then ith + 1 else ith;
-<%- end -%>
-]]>
-HERE
-  end
-
-  def eigen_val_inputs(i)
-    one_input("ans_val#{i}", "algebraic")
-  end
-
-  def eigen_ans_inputs(eigen_val_num, dim, input_size)
-    ret = ""
-    (1..eigen_val_num).each{|i|
-      ret << eigen_val_inputs(i)
-      ret << basis_ans(dim, dim, input_size, "#{i}_")
-    }
-    ret
-  end
-
-  def eigen_forms(eigen_val_num, dim)
-    ret = ""
-    (1..eigen_val_num).each{|idx|
-      ans = n_join(dim, "[[input:ans#{idx}_%d]]", " ")
-      valid = "[[validation:ans_val#{idx}]] "
-      valid += n_join(dim, "[[validation:ans#{idx}_%d]]", " ")
-      ret << ERB.new(<<HERE, nil, '-').result(binding)
-<p> 固有値 [[input:ans_val<%= idx %>]] それに対する固有ベクトルは次のベクトルの1次結合である <%= ans %></p>
-<div><%= valid %></div><br><br>
-HERE
-    }
-    ret
-  end
-
-  def basis_ans(n, dim, input_size, prefix="")
-    ret = ""
-    (1..n).each do |i|
-      ret << one_input("ans"+prefix+i.to_s, "matrix", dims: [dim, 1], input_size: input_size)
-    end
-    ret
-  end
-
-   def basis_feedback_lib_mac
- <<HERE.chomp
-#{does_hold_mac}
-is_same_linear_space(a, x) := block([ret, a0, x0, am, xm, am_dim, i],ret : true,a0 : listify(radcan(a)),x0 : listify(radcan(x)),am : apply(matrix, a0),xm : apply(matrix, x0),ret: ret and is(rank(am) = rank(xm)),if ret then (am_dim : rank(am),for i:1 thru length(x0) do (m : apply(matrix, cons(x0[i], a0)),ret : ret and is(rank(m) = am_dim))),ret);
-is_basis(x) := block([xm],xm : apply(matrix, x),is( rank(xm) = length(x) ));
-is_orthonormal_basis(x) := block([xm, tmp],xm : apply(matrix, radcan(x)),tmp : xm.(conjugate(transpose(xm))),does_hold( ident(length(x)) = tmp ) or does_hold( 1 = tmp ));
-HERE
-   end
  
   def plane_type_check(s, line_num)
     unless /\A\[[^\[\]]*?\]\z/ =~ s
@@ -583,6 +525,10 @@ class Eigen_multiplicity_eq < StackqBase
     @desc_varnames = [["固有値", "eigenval"], ["重複度", "chofuku"], ["固有空間の次元", "jigen"]]
   end
 
+  def eigen_val_num
+    @ans_num
+  end
+
   def eigen_multiplicity_num_dim(s)
     vecs = []
     arry = s.scan(/\[.*?\]/)
@@ -638,7 +584,38 @@ HERE
 
 end
 
+module BasisUtil
+
+  def basis_feedback_lib_mac
+<<HERE.chomp
+#{does_hold_mac}
+is_same_linear_space(a, x) := block([ret, a0, x0, am, xm, am_dim, i],ret : true,a0 : listify(radcan(a)),x0 : listify(radcan(x)),am : apply(matrix, a0),xm : apply(matrix, x0),ret: ret and is(rank(am) = rank(xm)),if ret then (am_dim : rank(am),for i:1 thru length(x0) do (m : apply(matrix, cons(x0[i], a0)),ret : ret and is(rank(m) = am_dim))),ret);
+is_basis(x) := block([xm],xm : apply(matrix, x),is( rank(xm) = length(x) ));
+is_orthonormal_basis(x) := block([xm, tmp],xm : apply(matrix, radcan(x)),tmp : xm.(conjugate(transpose(xm))),does_hold( ident(length(x)) = tmp ) or does_hold( 1 = tmp ));
+HERE
+  end
+
+  def basis_ans(n, dim, input_size, prefix="")
+    ret = ""
+    (1..n).each do |i|
+      ret << one_input("ans"+prefix+i.to_s, "matrix", dims: [dim, 1], input_size: input_size)
+    end
+    ret
+  end
+
+  def basis_chk(mthd)
+    case mthd
+    when /orthonormal/
+      "is_orthonormal_basis"
+    else
+      "is_basis"
+    end
+  end
+
+end
+
 class Is_basis_of_same_linear_space < StackqBase
+  include BasisUtil
 
   def initialize(*args)
     super
@@ -661,15 +638,6 @@ class Is_basis_of_same_linear_space < StackqBase
       ret << one_input("ans"+prefix+i.to_s, "matrix", dims: [@dim, 1], input_size: @input_size)
     end
     ret
-  end
-
-  def basis_feedback_lib_mac
-<<HERE.chomp
-#{does_hold_mac}
-is_same_linear_space(a, x) := block([ret, a0, x0, am, xm, am_dim, i],ret : true,a0 : listify(radcan(a)),x0 : listify(radcan(x)),am : apply(matrix, a0),xm : apply(matrix, x0),ret: ret and is(rank(am) = rank(xm)),if ret then (am_dim : rank(am),for i:1 thru length(x0) do (m : apply(matrix, cons(x0[i], a0)),ret : ret and is(rank(m) = am_dim))),ret);
-is_basis(x) := block([xm],xm : apply(matrix, x),is( rank(xm) = length(x) ));
-is_orthonormal_basis(x) := block([xm, tmp],xm : apply(matrix, radcan(x)),tmp : xm.(conjugate(transpose(xm))),does_hold( ident(length(x)) = tmp ) or does_hold( 1 = tmp ));
-HERE
   end
 
   def feedbk
@@ -713,6 +681,91 @@ HERE
         raise "invalid answer type"
       end
     }
+  end
+
+end
+
+class Is_same_eigenval_and_eigenvec < StackqBase
+include BasisUtil
+
+  def initialize(*args)
+    super
+    @eigen_val_num, @dim = eigen_num_dim(@a1)
+  end
+  attr_accessor :mthd
+  attr_reader :eigen_val_num
+
+  def basis_ans(n, dim, input_size, prefix="")
+    ret = ""
+    (1..n).each do |i|
+      ret << one_input("ans"+prefix+i.to_s, "matrix", dims: [dim, 1], input_size: input_size)
+    end
+    ret
+  end
+
+  def eigen_num_dim(s)
+    vecs = []
+    arry = s.scan(/\[(.*?), \[\s*((?:\[.*?\],?)+)\s*\]\s*\]/)
+    arry.each{|e|
+      vecs += e[1].scan(/\[.*?\]/).map{|s| s.split(",") }
+    }
+    vecs_sizes = vecs.map{|e| e.size }
+    unless vecs_sizes.uniq.size == 1
+      raise "the dims of eigen vectors are not the same"
+    end
+    dim = vecs_sizes[0]
+    eigen_val_num = arry.size
+    return *[eigen_val_num, dim]
+  end
+
+  def eigen_ans_arry(dim, idx)
+    "[" + n_join(dim, "ans#{idx}_%d") + "]"
+  end
+
+  def feedbk
+    ans_vals = n_join(@eigen_val_num, "ans_val%d") # ans_val == ans_eigenval
+    large_Ns = n_join(@dim, "N", ", ")
+    ERB.new(<<HERE, nil, '-').result(binding).chomp
+<![CDATA[
+<%= basis_feedback_lib_mac() %>
+ith : 0;
+result : is(<%= @eigen_val_num %> = length(unique([<%= ans_vals %>])));
+ith : if result then ith + 1 else ith;
+<%- (1..@eigen_val_num).each do |i| -%>
+vec<%= i %> : delete([<%= large_Ns %>], maplist(list_matrix_entries, <%= eigen_ans_arry(@dim,i) %>));
+kvec<%= i %> : assoc(ans_val<%= i %>, k1);
+result : result and listp(kvec<%= i %>) and <%= basis_chk(mthd) %>(vec<%= i %>) and is_same_linear_space(kvec<%= i %>, vec<%= i %>);
+ith : if result then ith + 1 else ith;
+<%- end -%>
+]]>
+HERE
+  end
+
+  def eigen_val_inputs(i)
+    one_input("ans_val#{i}", "algebraic")
+  end
+
+  def ans_inputs
+    ret = ""
+    (1..@eigen_val_num).each{|i|
+      ret << eigen_val_inputs(i)
+      ret << basis_ans(@dim, @dim, @input_size, "#{i}_")
+    }
+    ret
+  end
+
+  def ans_forms
+    ret = ""
+    (1..@eigen_val_num).each{|idx|
+      ans = n_join(@dim, "[[input:ans#{idx}_%d]]", " ")
+      valid = "[[validation:ans_val#{idx}]] "
+      valid += n_join(@dim, "[[validation:ans#{idx}_%d]]", " ")
+      ret << ERB.new(<<HERE, nil, '-').result(binding)
+<p> 固有値 [[input:ans_val<%= idx %>]] それに対する固有ベクトルは次のベクトルの1次結合である <%= ans %></p>
+<div><%= valid %></div><br><br>
+HERE
+    }
+    ret
   end
 
 end
