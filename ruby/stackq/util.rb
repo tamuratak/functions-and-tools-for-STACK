8de5ce844612506e4faab8d1aecb5cb420de7074
Takashi Tamura <tamuratak@users.noreply.github.com>
2017-01-09 17:45:02 +0900

    mv does_satisfy_ex to class Does_satisfy

--- ruby/stackq/util.rb
+++ ruby/stackq/util.rb
@@ -17,22 +17,6 @@ include ERB::Util
 include CDATAUtil
   module_function
 
-  def does_satisfy_ex(ext)
-    if /\A\(.*?\)\s*((and|or)\s*\(.*?\))*\z/ =~ ext
-      ext.gsub(/\((.*?)\)\s*(and|or|\z)/){|s|
-        e1 = $1
-        e2 = $2
-        if /\Anot (.*)/ =~ e1
-          "not does_hold(" + $1 + ") " + e2
-        else
-          "does_hold(" + e1 + ") " + e2
-        end
-      }
-    else
-      raise "format invalid for does_satisfy"
-    end
-  end
-
   def is_matrix_type(a)
     if /\Amatrix/ =~ a
       a = a.gsub(/\s+/, "")
